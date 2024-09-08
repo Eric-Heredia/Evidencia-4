e7cbfb11a0a6e081daf99fa444d43eedad1e9244
Eric-Heredia <ericvheredia@gmail.com>
2024-09-08 20:32:35 -0300

    Se prueban los test y se realizan ajustes en función de los resultados

--- MaquinaFototerapia.py
+++ MaquinaFototerapia.py
@@ -4,7 +4,6 @@ class MaquinaFototerapia:
         self.intensidad = 0
         self.potencia_maxima = potencia_maxima
         self.tiempo_sesion = 0
-        self.usuario_actual = None
 
     def encender(self):
         self.encendida = True
@@ -16,16 +15,7 @@ class MaquinaFototerapia:
         if 0 <= nueva_intensidad <= self.potencia_maxima:
             self.intensidad = nueva_intensidad
         else:
-            print("Intensidad fuera de rango.")
-
-    def iniciar_sesion(self, usuario):
-        # Simulación básica de autenticación
-        usuarios_validos = {"usuario1": "contraseña1", "usuario2": "contraseña2"}
-        if usuario in usuarios_validos:
-            self.usuario_actual = usuario
-            print(f"Usuario {usuario} inició sesión.")
-        else:
-            print("Usuario o contraseña incorrectos.")
+            raise ValueError("Intensidad fuera de rango.")
 
     def obtener_estado(self):
         return {
@@ -33,8 +23,7 @@ class MaquinaFototerapia:
             "intensidad": self.intensidad,
             "potencia_maxima": self.potencia_maxima,
             "tiempo_sesion": self.tiempo_sesion,
-            "usuario_actual": self.usuario_actual
         }
 
     def __str__(self):
-        return f"Máquina de fototerapia: Encendida: {self.encendida}, Intensidad: {self.intensidad}, Potencia máxima: {self.potencia_maxima}, Usuario actual: {self.usuario_actual}"
+        return f"Máquina de fototerapia: Encendida: {self.encendida}, su Intensidad es: {self.intensidad}, y la Potencia máxima: {self.potencia_maxima}"
